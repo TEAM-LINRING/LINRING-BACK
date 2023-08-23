bec06149da77137c89f4917835787e7d7d4e83a3
rktlskan021 <rktlskan021@gmail.com>
2023-08-24 00:50:16 +0900

    feat: 이메일 로그인 구현3
    기존 회원가입 기능을 가진 signup 관련 삭제
    회원탈퇴 기능 구현

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
-    path("signup/", views.signup, name="signup"),
     path("registration/", include('dj_rest_auth.registration.urls')),
+    path("delete_success/", views.userDelete, name="delete_success")
 ]
--- users/views.py
+++ users/views.py
@@ -4,16 +4,15 @@ from django.contrib import auth
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+
 
 # Create your views here.
-def signup(request):
-    if request.method == 'POST':
-        if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(
-                username=request.POST['username'],
-                password=request.POST['password1'],
-                email=request.POST['email'],)
-            auth.login(request, user)
-            return redirect('/')
-        return render(request, 'users/signup.html')
-    return render(request, 'users/signup.html')
+def userDelete(request):
+    if request.user.is_authenticated:
+        user = request.user
+        print(user.email)
+        user.delete()
+        logout(request)
+    context = {}
+    return render(request, 'users/delete_success.html', context)
